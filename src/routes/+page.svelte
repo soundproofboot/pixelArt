6bae5240027cff27a330d5df5d20115c7237f438
Colin Bares <colinedwinbares@gmail.com>
2023-03-13 16:15:14 -0500

    removed changing size of canvas, forced square canvas, style tool bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,5 @@
 <script lang=ts>
-    function getRandomColor() {
-        let stringChars = ['0','1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f']
-        let colorString = '#';
-        for (let i = 0; i < 6; i++) {
-            colorString += stringChars[Math.floor(Math.random() * stringChars.length)];
-        }
-        return colorString;
-    }
+    import { onMount } from "svelte";
 
     let grid: any[] = [];
     let brushColor: string = '#000000';
@@ -22,10 +15,23 @@
     $: containerStyle = `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`
     $: down = false;
     $: showGrid = true;
+    let colorPicker: HTMLElement;
+    $: colorPickerStyle = '';
+
+    onMount(() => {
+        let buttonEl = document.querySelector('button');
+        let dimensions = buttonEl?.getBoundingClientRect();
+        colorPickerStyle = `width: ${dimensions?.width}px; height: ${dimensions?.height}px`;
+    })
 </script>
 
 <svelte:window 
-    on:mousedown={(e) => {e.preventDefault(); down = true}}
+    on:mousedown={(e) => {
+        if (e.target !== colorPicker) {
+            e.preventDefault();
+        }
+        down = true
+        }}
     on:mouseup={() => down = false}
     on:touchstart={(e) => {e.stopPropagation(); down = true;}}
     on:touchend={()=> down = false}
@@ -34,92 +40,61 @@
         let y = e.touches[0].clientY;
 
         let copy = [...grid];
-                let el = copy[0][0].el;
-                let { top, bottom, left, right } = copy[0][0].el.getBoundingClientRect();
-                el.getBoundingClientRect().top;
-                for (let i = 0; i < copy.length; i++) {
-                    for (let j = 0; j < copy[i].length; j++) {
-                        let {top, bottom, left, right } = copy[i][j].el.getBoundingClientRect();
-                if (y < bottom && y > top && x > left && x < right) {
-                            if (down) copy[i][j].color = brushColor;
-                        }
+            for (let i = 0; i < copy.length; i++) {
+                for (let j = 0; j < copy[i].length; j++) {
+                    let {top, bottom, left, right } = copy[i][j].el.getBoundingClientRect();
+            if (y < bottom && y > top && x > left && x < right) {
+                        if (down) copy[i][j].color = brushColor;
                     }
-                 }
-                grid = copy;
+                }
+                }
+            grid = copy;
     }}
     />
-<div class=page style={containerStyle}>
-    {#each grid as row, i}
+<div style="height: 100%; display: flex; flex-direction: column; justify-content: space-evenly;">
+    <div class=canvas style={containerStyle}>
+        {#each grid as row, i}
         {#each row as box, j}
         <div 
-            style="background-color: {box.color}"
-            class={showGrid ? 'gridLines' : ''}
-            id='row{i}box{j}'
-            bind:this={box.el}
-            on:mousedown={() => box.color = brushColor}
-            on:touchstart={() => box.color = brushColor}
-            on:mouseenter={() => {
-                if (down) {
-                    box.color = brushColor;
-                }
-            }}
+        style="background-color: {box.color};"
+        class={showGrid ? 'gridLines' : ''}
+        id='row{i}box{j}'
+        bind:this={box.el}
+        on:mousedown={() => box.color = brushColor}
+        on:touchstart={() => box.color = brushColor}
+        on:mouseenter={() => {
+            if (down) {
+                box.color = brushColor;
+            }
+        }}
             >
         </div>
         {/each}
-    {/each}
-</div>
-<div class=toolbar>
-    <div>
-        <span style="color: white;">Color: </span>
-        <input type=color bind:value={brushColor}>
+        {/each}
     </div>
-    <div>
-        <button
-            on:click={() => {
-                grid = grid.map(row => {
-                    let newRow = row.map(box => {
-                        return { color: 'white'}
-                    });
-                    return newRow;
-                })
-            }}
-        >Reset</button>
-        <button
-            on:click={() => {
-                showGrid = !showGrid
-            }}
+    <div class=toolbar >
+        <input 
+        in:fade={{duration: 1000}}
+        type=color 
+        bind:value={brushColor} 
+        bind:this={colorPicker}
+        style={colorPickerStyle}
         >
-            Show Grid
-        </button>
         <button
+        class=resetButton
         on:click={() => {
-        
             grid = grid.map(row => {
-                return row.map(box => {
-                    return { color: getRandomColor()}
-                })
+                let newRow = row.map(box => {
+                    return { color: 'white'}
+                });
+                return newRow;
             })
         }}
-        >Random</button>
+        >Reset</button>
     </div>
-    <div>
-        <span style="color: white;">Grid size: {size}</span>
-        <button on:click={() => {
-            grid = grid.map(row => [...row, { color: 'white'}]);
-            let newRow = [];
-            for (let i = 0; i < size +1; i++) {
-                newRow.push({ color: 'white'})
-            }
-            grid = [...grid, newRow]
-            size++
-            }}>up</button>
-        <button on:click={() => {
-            grid = grid.map(row => row.slice(0, -1))
-            grid=grid.slice(0, -1)
-            size--
-            }}>down</button>
-    </div>
+
 </div>
+
 <style>
     :global(body, html) {
         background-color: black;
@@ -129,19 +104,58 @@
         padding: 0;
         box-sizing: border-box;
     }
+    
+    :global(body) {
+        overflow: hidden;
+        position: fixed;
+    }
 
-    .page {
+    .canvas {
         background-color: white;
-        height: 90%;
-        width: 100%;
         display: grid;
+        margin: 1em auto;
+        outline: 5px solid gray;
+        border-radius: 5px;
+    }
+
+    @media (orientation: landscape) {
+        .canvas {
+           height: 80vh;
+           width: 80vh;
+        }
+
+        .resetButton {
+            height: 50%;
+            aspect-ratio: 1/1;
+        }
+    }
+
+    @media (orientation: portrait) {
+        .canvas {
+           height: 90vw;
+           width: 90vw;
+        }
+
+        .resetButton {
+            width: 20vw;
+            aspect-ratio: 1/1;
+        }
     }
 
     .toolbar {
-        height: 10%;
         display: flex;
-        justify-content: space-evenly;
+        justify-content: center;
         align-items: center;
+        column-gap: 2em;
+        flex-grow: 1;
+    }
+
+    .resetButton {
+        background-color: transparent;
+        border: none;
+        color: white;
+        outline: 2px solid white;
+        border-radius: 5px;
     }
 
     .gridLines {
